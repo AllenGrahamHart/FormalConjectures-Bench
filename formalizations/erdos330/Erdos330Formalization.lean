f74eaa8db901f5455d031e6217587f1ebde41339
AllenGrahamHart <allenhart094@googlemail.com>
2026-05-04 21:00:33 +0100

    Add Erdős 330 stage coverage wrappers

--- formalizations/erdos330/Erdos330Formalization.lean
+++ formalizations/erdos330/Erdos330Formalization.lean
@@ -13,3 +13,4 @@ import Erdos330Formalization.AffineSafePairs
 import Erdos330Formalization.ProductGadget
 import Erdos330Formalization.ConcreteCRTGadget
 import Erdos330Formalization.LocalReservoir
+import Erdos330Formalization.StageCoverage
